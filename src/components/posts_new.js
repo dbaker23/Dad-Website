fe0b4cdfddaf3db3f8743b2a4c687c78f28965e2
Derek <derekjohnbaker23@gmail.com>
2018-11-05 16:13:59 -0600

    1.0

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component { 
     renderField( field ) {
@@ -20,8 +23,10 @@ class PostsNew extends Component {
         );
     }
 
-    onSubmit( values ) {
-        console.log( values );
+    onSubmit( values ) {        
+        this.props.createPost( values, () => {
+            this.props.history.push( '/' );
+        });
     }
 
     render() {
@@ -45,6 +50,7 @@ class PostsNew extends Component {
                     component={this.renderField}
                 />
                 <button type='submit' className='btn btn-primary'>Submit</button>
+                <Link to='/' className='btn btn-danger'>Cancel</Link>
             </form>
         );
     }
@@ -74,4 +80,6 @@ function validate( values ) {
 export default reduxForm({
     validate,
     form: 'PostsNewForm'
-})( PostsNew );
+})( 
+    connect( null, {createPost} )( PostsNew ) // layer up to use forms and connects together
+);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,10 +1,22 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 import _ from 'lodash';
 
 export default function( state = {}, action ) {
-    switch( action.type ) {
+    switch( action.type ) {        
+        case DELETE_POST:
+            return _.omit( state, action.payload );
+
         case FETCH_POSTS:
             return _.mapKeys( action.payload.data, 'id' );
+
+        case FETCH_POST:
+            // const post = action.payload.data;
+            // const newState = { ...state };
+            // newState[ post.id ] = post;
+            // return newState;
+
+            //identical to above
+            return { ...state, [action.payload.data.id]: action.payload.data };
         default:
             return state;
     }
